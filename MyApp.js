35b38951fa5322dd28fc9b5fbd0ffa9f4085483d
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2023-12-14 16:23:43 +0200

    Add hooks.

--- MyApp.js
+++ MyApp.js
@@ -1,11 +1,17 @@
 function Header({
   title
 }) {
+  console.log("Header is rendering.");
+  const [classHeader, setClassHeader] = React.useState("app-header-aqua");
+  React.useEffect(() => {
+    console.log('useEffect hook');
+  }, []);
   const clickHandler = e => {
-    console.log('click me!');
+    console.log('click me!, Change to Red Header');
+    setClassHeader("app-header-red");
   };
   return /*#__PURE__*/React.createElement("div", {
-    className: "app-header",
+    className: classHeader,
     onClick: clickHandler
   }, " Header: ", title, " ");
 }
--- MyApp.jsx
+++ MyApp.jsx
@@ -1,15 +1,23 @@
 
 function Header({ title }) {
+    console.log("Header is rendering.");
 
-    const clickHandler = (e) => { 
-        console.log('click me!');
-    }
+    const [classHeader, setClassHeader] = React.useState("app-header-aqua");
 
-    return <div className="app-header" onClick={clickHandler}> Header: {title} </div>
+    React.useEffect(() => {
+        console.log('useEffect hook');
+    }, []);
+
+    const clickHandler = (e) => {
+        console.log('click me!, Change to Red Header');
+        setClassHeader("app-header-red");
+    };
+    
+    return <div className={classHeader} onClick={clickHandler}> Header: {title} </div>
 }
 
 function MyApp() {
     return (<>
         <Header title={'Hello React Title'}></Header><div>Welcome to React Pre Process code page</div>
-        </>)
+    </>)
 }
