5b8a1bb907694b2c282dab46e56fc2b9d674bc99
vlad <vlad19972017@gmail.com>
2023-12-30 13:42:23 +0200

    change logic

--- MyApp.js
+++ MyApp.js
@@ -16,11 +16,89 @@ function Header({
     onClick: clickHandler
   }, " Header: ", title, ",", /*#__PURE__*/React.createElement("div", null, "Children: ", children));
 }
+const employmentEnum = {
+  FullTime: 'full-time',
+  PartTime: 'part-time'
+};
 function MyApp() {
+  const formData = {
+    firstName: "Vladyslav",
+    employment: employmentEnum.PartTime,
+    comments: "long comments",
+    isFriendly: true
+  };
+
+  // const [formData, setForm] = React.useState({
+  //     firstName: "Vladyslav",
+  //     employment: employmentEnum.PartTime,
+  //     comments: "long comments",
+  //     isFriendly: true
+  // });
+
+  handlerChanges = $event => {
+    const {
+      name,
+      value,
+      type,
+      checked
+    } = $event.target;
+    console.log("name: ", name);
+    console.log("value: ", value);
+    console.log("type: ", type);
+    console.log("checked: ", checked);
+
+    // setForm(prevFromData => ({
+    //     ...prevFromData,
+    //     [name]: type === "checkbox" ? checked : value
+    // }));
+  };
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Header, {
     title: 'Hello React Title'
   }, /*#__PURE__*/React.createElement("span", {
     className: "app-span"
-  }, "Hello from patent")), /*#__PURE__*/React.createElement("div", null, "Welcome to React Pre Process code page"));
+  }, "Hello from patent")), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("input", {
+    id: "firstName_id",
+    name: "firstName",
+    placeholder: "First Name",
+    type: "text",
+    onChange: handlerChanges
+  }), /*#__PURE__*/React.createElement("textarea", {
+    id: "comments_id",
+    name: "comments",
+    placeholder: "Comments",
+    value: formData.comments,
+    onChange: handlerChanges
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "isFriendly_id"
+  }, "Is Friendly"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+    id: "isFriendly_id",
+    type: "checkbox",
+    name: "isFriendly",
+    checked: formData.isFriendly,
+    onChange: handlerChanges
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "employment_id_part-time"
+  }, "Part Time"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+    type: "radio",
+    name: "employment",
+    value: employmentEnum.PartTime,
+    id: "employment_id_part-time",
+    checked: formData.employment === employmentEnum.PartTime,
+    onChange: handlerChanges
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "employment_id_full-time"
+  }, "Full Time"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+    type: "radio",
+    name: "employment",
+    value: employmentEnum.FullTime,
+    id: "employment_id_full-time",
+    checked: formData.employment === employmentEnum.FullTime,
+    onChange: handlerChanges
+  }), /*#__PURE__*/React.createElement("button", {
+    onClick: e => {
+      console.log("Data from formData.");
+      console.log(form);
+    }
+  }, " Submit ")), /*#__PURE__*/React.createElement("div", null, "Welcome to React Pre Process code page"));
 }
 
--- MyApp.jsx
+++ MyApp.jsx
@@ -17,11 +17,99 @@ function Header({ title, children }) {
         </div>
 }
 
+
+const employmentEnum = {
+    FullTime: 'full-time',
+    PartTime: 'part-time'
+}
+
 function MyApp() {
+
+    const formData = {
+            firstName: "Vladyslav",
+            employment: employmentEnum.PartTime,
+            comments: "long comments",
+            isFriendly: true
+    }
+
+    // const [formData, setForm] = React.useState({
+    //     firstName: "Vladyslav",
+    //     employment: employmentEnum.PartTime,
+    //     comments: "long comments",
+    //     isFriendly: true
+    // });
+
+    handlerChanges = ($event) => {
+        const { name, value, type, checked } = $event.target;
+        console.log("name: ", name);
+        console.log("value: ", value);
+        console.log("type: ", type);
+        console.log("checked: ", checked);
+
+        // setForm(prevFromData => ({
+        //     ...prevFromData,
+        //     [name]: type === "checkbox" ? checked : value
+        // }));
+    }
+
+
     return (<>
         <Header title={'Hello React Title'}>
             <span className="app-span">Hello from patent</span>
         </Header>
+        <div>
+            <input
+                id="firstName_id"
+                name="firstName"
+                placeholder="First Name"
+                type="text"
+                onChange={handlerChanges}
+            />
+
+            <textarea
+                id="comments_id"
+                name="comments"
+                placeholder="Comments"
+                value={formData.comments}
+                onChange={handlerChanges}
+            />
+
+            <label htmlFor="isFriendly_id">Is Friendly</label>
+            <br />
+            <input
+                id="isFriendly_id"
+                type="checkbox"
+                name="isFriendly"
+                checked={formData.isFriendly}
+                onChange={handlerChanges}
+            />
+
+            <label htmlFor="employment_id_part-time">Part Time</label>
+            <br />
+            <input
+                type="radio"
+                name="employment"
+                value={employmentEnum.PartTime}
+                id="employment_id_part-time"
+                checked={formData.employment === employmentEnum.PartTime}
+                onChange={handlerChanges} />
+
+            <label htmlFor="employment_id_full-time">Full Time</label>
+            <br />
+            <input
+                type="radio"
+                name="employment"
+                value={employmentEnum.FullTime}
+                id="employment_id_full-time"
+                checked={formData.employment === employmentEnum.FullTime}
+                onChange={handlerChanges} />
+            
+            <button onClick={(e) => {
+                console.log("Data from formData.");
+                console.log(form);
+            }}> Submit </button>
+            
+        </div>
         <div>Welcome to React Pre Process code page</div>
     </>)
 }
